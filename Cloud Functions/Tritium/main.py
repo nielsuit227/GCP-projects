1f63df403fe28584b05bd84b90bc54aa05610a9c
Niels Uitterdijk <niels.uit@gmail.com>
2021-02-04 14:48:43 +0100

    v1.0.0

--- Cloud Functions/Tritium/main.py
+++ Cloud Functions/Tritium/main.py
@@ -1,11 +1,13 @@
-import json, joblib, pickle, cantools, re
+import json, joblib, pickle, cantools, re, logging
 import numpy as np
 import pandas as pd
+from io import StringIO
 from google.cloud import storage
 
 # Global (avoids reloading, makes hot calls cheaper :)
 model = None
 normalization = None
+input_features = None
 features = None
 dbc = None
 bucket = 'amplo-storage'
@@ -28,7 +30,7 @@ def load_file(file_loc):
         bucket_class = client.get_bucket(bucket)
     except Exception as e:
         print('Connecting to bucket %s failed.' % bucket)
-        print(e)
+        logging.error(e)
         return error_message
     # Get file
     try:
@@ -36,7 +38,7 @@ def load_file(file_loc):
         blob.download_to_filename('/tmp/' + file_loc)
     except Exception as e:
         print('Loading %s into memory failed.' % file_loc)
-        print(e)
+        logging.error(e)
         return error_message
 
 
@@ -45,7 +47,7 @@ def predict_iso(request):
     Automated Diagnostics API for Tritium.
     Triggered with HTTPS Post. Requires API key for authentication and csv for prediction.
     """
-    global model, features, normalization, dbc, api_key, error_message
+    global model, input_features, features, normalization, dbc, api_key, error_message
 
     # Checks
     if request.method != 'POST':
@@ -57,27 +59,37 @@ def predict_iso(request):
     # todo check csv
 
     # Load Prediction files
-    if model is None:
-        load_file('model.joblib')
-        model = joblib.load('/tmp/model.joblib')
-    if features is None:
-        load_file('features.json')
-        features = json.load(open('/tmp/features.json', 'r'))
-    if normalization is None:
-        load_file('normalization.pickle')
-        normalization = pickle.load(open('/tmp/normalization.pickle', 'rb'))
-    if dbc is None:
-        load_file('tri93-rt.dbc')
-        dbc = cantools.database.load_file('/tmp/tri93-rt.dbc')
+    try:
+        if model is None:
+            load_file('model.joblib')
+            model = joblib.load('/tmp/model.joblib')
+        if input_features is None:
+            load_file('input_features.json')
+            input_features = json.load(open('/tmp/input_features.json', 'r'))
+        if features is None:
+            load_file('features.json')
+            features = json.load(open('/tmp/features.json', 'r'))
+            # todo validate working of normalization with sklearn
+        if normalization is None:
+            load_file('normalization.pickle')
+            normalization = pickle.load(open('/tmp/normalization.pickle', 'rb'))
+        if dbc is None:
+            load_file('tri93-rt.dbc')
+            dbc = cantools.database.load_file('/tmp/tri93-rt.dbc')
+    except Exception as e:
+        print('Loading files failed.')
+        logging.error(e)
+        return error_message
 
     # Load CSV
     try:
-        df = pd.read_csv(request.file.get('file'))
+        print('Bits: ', request.data)
+        df = pd.read_csv(StringIO(request.data.decode('utf-8')))
         for key in df.keys():
             df = df.rename(columns={key: key.replace(' ', '')})
     except Exception as e:
         print('File not loaded')
-        print(e)
+        logging.error(e)
         return error_message
 
     # Data check
@@ -89,28 +101,32 @@ def predict_iso(request):
     # Decode
     try:
         dec_list = []
-        ind_list = [m.frame_id for m in dbc.messages]
+        print('Received data keys:', df.keys())
         for i in range(10000):
             row = df.iloc[i]
-            if row['ID'] in ind_list:
+            try:
                 dec = dbc.decode_message(int(row['ID'], 0), row['data'].encode('ascii'))
                 dec['ts'] = row['Recvtime']
                 dec_list.append(dec)
+            except:
+                pass
         data = pd.DataFrame(dec_list)
+        del dec_list, row, df
     except Exception as e:
         print('Decoding failed.')
-        print(e)
+        logging.error(e)
         return error_message
 
     # Convert to timeseries
     try:
+        print('Timeseries starting keys:', data.keys())
         data = data.set_index(pd.to_datetime(data['ts']))
         data = data.mean(level=0)
         data = data.resample('ms').interpolate(limit_direction='both')
         data = data.resample('s').asfreq()
     except Exception as e:
         print('Conversion to timeseries failed.')
-        print(e)
+        logging.error(e)
         return error_message
 
     # Cleaning Keys
@@ -120,9 +136,10 @@ def predict_iso(request):
             new_keys[key] = re.sub('[^a-zA-Z0-9]', '_', key.lower())
         data = data.rename(columns=new_keys)
         data = data.loc[:, ~data.columns.duplicated()]
+        del new_keys
     except Exception as e:
         print('Key Cleaning Failed.')
-        print(e)
+        logging.error(e)
         return error_message
 
     # Categorical Variables
@@ -133,22 +150,26 @@ def predict_iso(request):
             for dummy_key in dummies.keys():
                 dummies = dummies.rename(columns={dummy_key: key + '_' + str(dummy_key)})
             data = data.drop(key, axis=1).join(dummies)
+        del dummies, key
     except Exception as e:
         print('Categorical Variables Failed')
-        print(e)
+        logging.error(e)
         return error_message
 
     # Normalizing
     try:
-        data = normalization.convert(data)
+        for key in [x for x in input_features if x not in list(data.keys())]:
+            data.loc[:, key] = np.zeros(len(data))
+        data = data[input_features]
+        data[data.keys()] = normalization.transform(data)
     except Exception as e:
         print('Normalization Failed.')
-        print(e)
+        logging.error(e)
         return error_message
 
-    # Decoded data check
-    if not set(features).issubset(set(data.keys())):
-        return 'Required features for prediction missing, please contact info@amplo.ch'
+    # Decoded data check - Deprecated, zeros added instead
+    # if not set(features).issubset(set(data.keys())):
+    #     return 'Required features for prediction missing, please contact info@amplo.ch'
 
     # Select features
     try:
@@ -157,7 +178,7 @@ def predict_iso(request):
         data = data[features]
     except Exception as e:
         print('Selecting features failed.')
-        print(e)
+        logging.error(e)
         return error_message
 
     # Making predictions
@@ -166,7 +187,7 @@ def predict_iso(request):
         return 'Probability of faulty ISO Board: %.2f %%' % min(100, max(0, (sum(predictions) / len(predictions) * 50 + 50)))
     except Exception as e:
         print('Making predictions failed.')
-        print(e)
+        logging.error(e)
         return error_message
 
     # todo store prediction
